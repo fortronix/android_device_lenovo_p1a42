94f0f2e4a84dedf1686cbd76a73ab9cb821ec9bf
FireLord1 <firelord.xda@gmail.com>
2016-10-18 20:18:02 +0530

    p1a42: Enable overlays
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- device.mk
+++ device.mk
@@ -1,6 +1,9 @@
 # Local Path
 LOCAL_PATH := device/lenovo/p1a42
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
 # Ramdisk
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
